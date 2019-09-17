8a5c6e2a3b53304a2d4ae9c2a45835119531d98e
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-09-17 16:32:47 -0400

    Login functionality set up.

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -9,19 +9,43 @@ class Login extends React.Component {
         }
     };
 
+    login = e => {
+        e.preventDefault();
+        axiosWithAuth() // no need to attach local host to URL because that's already done in the function call
+            .post('/login', this.state.credentials)
+            .then(res => {
+                localStorage.setItem('token', res.data.payload);
+                this.props.history.push('/protected');
+            })
+            .catch(err => console.log(err))
+    }
+
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials, 
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.login}>
                     <input
                         type="text"
                         name="username"
                         placeholder="Username"
+                        onChange={this.handleChange}
+                        value={this.state.credentials.username}
                     />
                     <input 
                         type="password"
                         name="password"
                         placeholder="Password"
+                        onChange={this.handleChange}
+                        value={this.state.credentials.password}
                     />
                     <button>Log In</button>
                 </form>
